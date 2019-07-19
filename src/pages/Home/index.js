a7de004a36c6ba83972e11cdf9fade9101e7d051
Manoel <manoel.lobo@gmail.com>
2019-07-19 16:06:19 -0300

    WIP: style Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
 
 export default function Home() {
   return (
-    <View>
+    <Container>
       <Text>HOME</Text>
-    </View>
+    </Container>
   );
 }
 
