062ca3f0ea443dd8b9fae1640aec65d0bd02b22a
Manoel <manoel.lobo@gmail.com>
2019-07-22 11:23:26 -0300

    Home page fetches products

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import api from '../../services/api';
 import { Container } from './styles';
 
-export default function Home() {
-  return (
-    <Container>
-      <Text>HOME</Text>
-    </Container>
-  );
+export default class Home extends React.Component {
+  state = { products: [] };
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+
+    this.setState({ products: response.data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <Container>
+        {products.length > 0 ? (
+          products.map(product => (
+            <Text key={product.id.toString()}>{product.title}</Text>
+          ))
+        ) : (
+          <Text>NO</Text>
+        )}
+      </Container>
+    );
+  }
 }
 
 Home.navigationOptions = {
